ae0163af7e029d786f35847e994a44fea236c138
Jadon Lau <jadon@dhcp-10-105-19-29.wireless.northwestern.private>
2023-10-10 12:16:51 -0500

    added on/off camera and on/off mic

--- src/CameraComponent.js
+++ src/CameraComponent.js
@@ -1,16 +1,52 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import Webcam from 'react-webcam';
 
 const CameraComponent = () => {
   const webcamRef = useRef(null);
+  const [isCameraOn, setIsCameraOn] = useState(true);
+  const [isMicOn, setIsMicOn] = useState(true);
+  const [key, setKey] = useState(Date.now()); // Key to re-render Webcam
+
+  const toggleCamera = () => {
+    const { current } = webcamRef;
+    if (isCameraOn && current && current.stream) {
+      const videoTracks = current.stream.getVideoTracks();
+      videoTracks.forEach((track) => {
+        track.stop();
+      });
+    } else {
+      setKey(Date.now()); // Force re-render to start the camera again
+    }
+    setIsCameraOn(!isCameraOn);
+  };
+
+  const toggleMic = () => {
+    const { current } = webcamRef;
+    if (isMicOn && current && current.stream) {
+      const audioTracks = current.stream.getAudioTracks();
+      audioTracks.forEach((track) => {
+        track.stop();
+      });
+    } else {
+      setKey(Date.now()); // Force re-render to start the mic again
+    }
+    setIsMicOn(!isMicOn);
+  };
 
   return (
     <div>
       <Webcam
-        audio={false}
+        key={key}
+        audio={isMicOn}
         ref={webcamRef}
-        mirrored={true} // Set to true to mirror the camera view
+        mirrored={true} 
       />
+      <button onClick={toggleCamera}>
+        {isCameraOn ? "Turn Camera Off" : "Turn Camera On"}
+      </button>
+      <button onClick={toggleMic}>
+        {isMicOn ? "Turn Mic Off" : "Turn Mic On"}
+      </button>
     </div>
   );
 };
