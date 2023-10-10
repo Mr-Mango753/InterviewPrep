ee990998d353d84f714b1af0ba35c1b0f1675643
Jadon Lau <jadon@dhcp-10-105-19-29.wireless.northwestern.private>
2023-10-10 12:08:39 -0500

    add start interview button

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Webcam from 'react-webcam';
 import CameraComponent from './CameraComponent';
+import Button from './components/Button.jsx'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <CameraComponent />
+        <Button />
       </header>
     </div>
   );
---
+++ src/components/Button.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Button.css';
+
+function Button() {
+  return (
+    <button type="button" className="custom-btn">
+      Join Interview
+    </button>
+  );
+}
+
+export default Button;
