461886ead1df364a1b2ee17649b058a786643b47
Jadon Lau <jadon@dhcp-10-105-19-29.wireless.northwestern.private>
2023-10-12 11:31:23 -0500

    voice to text

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Webcam from 'react-webcam';
 import CameraComponent from './CameraComponent';
 import Button from './components/Button.jsx'
+import VoiceToText from './components/VoiceToText';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <header className="App-header">
         <CameraComponent />
         <Button />
+        <VoiceToText />
       </header>
     </div>
   );
---
+++ src/components/VoiceToText.jsx
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+
+const VoiceToText = () => {
+    const [transcript, setTranscript] = useState('');
+    const [isListening, setIsListening] = useState(false);
+
+    const handleStart = () => {
+        if (window.SpeechRecognition || window.webkitSpeechRecognition) {
+            const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+            const recognition = new SpeechRecognition();
+            
+            recognition.onstart = () => setIsListening(true);
+            recognition.onresult = (event) => {
+                setTranscript(event.results[0][0].transcript);
+                setIsListening(false);
+            };
+            recognition.onend = () => setIsListening(false);
+
+            recognition.start();
+        } else {
+            alert("Your browser does not support the Web Speech API. Please try a different browser.");
+        }
+    };
+
+    return (
+        <div>
+            <button onClick={handleStart}>
+                {isListening ? 'Listening...' : 'Start Listening'}
+            </button>
+            <textarea value={transcript} readOnly placeholder="Transcription will appear here..."></textarea>
+        </div>
+    );
+}
+
+export default VoiceToText;
