f2061d61f802262646f6af9678483ff9a1ce73a7
perrybenyella <perrybenyella2024@u.northwestern.edu>
2023-10-23 14:53:42 -0500

    started questions

---
+++ src/Questions.py
@@ -0,0 +1,10 @@
+programming_problems = {
+    "Two Pointers" : [
+        ["Valid Palindrome", "125"],
+        ["Two Sum II",
+         "3Sum",
+         "Container With Most Water",],
+        ["Trap Rain Water"],
+    ],
+    
+}
