af749443fa7b8608680b4eacd96c8a2685d942e0
Jadon <jadonlau2024@u.northwestern.edu>
2023-10-24 20:57:43 -0500

    nothing changed

--- flask-server/venv/server.py
+++ flask-server/venv/server.py
@@ -3,14 +3,20 @@ from flask_cors import CORS
 from transformers import AutoProcessor, BarkModel
 import scipy
 import os
+import numpy as np
 
 app = Flask(__name__, static_folder="audio_files")
-cors = CORS(app, resources={r"/generate_audio": {"origins": "http://localhost:3000"}})
+# cors = CORS(app, resources={r"/generate_audio": {"origins": "http://localhost:3000"}})
+
+# CORS(app, supports_credentials=True)
+CORS(app, resources={r"/*": {"origins": "*"}})
+
+
 
 # CORS(app)
 # os.environ['SUNO_USE_SMALL_MODELS'] = 'True'
-os.environ["SUNO_OFFLOAD_CPU"] = "True"
-os.environ["SUNO_USE_SMALL_MODELS"] = "True"
+# os.environ["SUNO_OFFLOAD_CPU"] = "True"
+# os.environ["SUNO_USE_SMALL_MODELS"] = "True"
 processor = AutoProcessor.from_pretrained("suno/bark")
 model = BarkModel.from_pretrained("suno/bark")
 AUDIO_FOLDER = "audio_files"
@@ -18,6 +24,31 @@ print(os.path.abspath(AUDIO_FOLDER))
 
 
 @app.route('/generate_audio', methods=['POST'])
+# def generate_audio():
+#     try:
+#         text = request.json['text']
+#         voice_preset = "v2/en_speaker_6"
+        
+#         # Split text into chunks based on word count
+#         words = text.split()
+#         chunk_size = 15  # You can adjust this value based on the maximum size that works well with the model
+#         chunks = [' '.join(words[i:i + chunk_size]) for i in range(0, len(words), chunk_size)]
+
+#         combined_audio = []
+
+#         for chunk in chunks:
+#             inputs = processor(chunk, voice_preset=voice_preset)
+#             audio_array_chunk = model.generate(**inputs).cpu().numpy().squeeze()
+#             combined_audio.extend(audio_array_chunk.tolist())
+        
+#         output_filename = os.path.join(AUDIO_FOLDER, f"audio1.wav")
+#         sample_rate = model.generation_config.sample_rate
+#         scipy.io.wavfile.write(output_filename, rate=sample_rate, data=np.array(combined_audio, dtype=np.int16))
+        
+#         return jsonify({"audio_file": os.path.basename(output_filename)})
+
+#     except Exception as e:
+#         return jsonify({"error": str(e)}), 500
 def generate_audio():
     try:
         text = request.json['text']
@@ -32,21 +63,15 @@ def generate_audio():
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
-@app.route('/audio_files/<filename>', methods=['GET'])
-def serve_audio_file(filename):
+@app.route('/audio1', methods=['GET'])
+def serve_audio_file():
     """Serve the audio file to the frontend."""
-    return send_from_directory(AUDIO_FOLDER, filename)
+    return send_from_directory(app.static_folder, "audio1.wav")
 
-# @app.route('/test_audio', methods=['GET'])
-# def test_audio():
-#     """Serve a specific audio file."""
-#     return send_from_directory(AUDIO_FOLDER, "audio1.wav")
 
-@app.route('/simple_test')
-def simple_test():
-    """Serve a specific audio file directly."""
-    return send_from_directory("/Users/jadon/Desktop/Fall 2023/338/my-app/audio_files", "audio1.wav")
-
+@app.route('/test_cors', methods=['GET', 'OPTIONS'])
+def test_cors():
+    return "CORS test", 200
 
 if __name__ == '__main__':
     # Ensure the audio directory exists
